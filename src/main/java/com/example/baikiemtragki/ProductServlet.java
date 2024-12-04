46ed210245771a319757b2f43675047b3858c986
phuonganh <phuonganh241976@gmail.com>
2024-12-04 13:53:12 +0700

    he

---
+++ src/main/java/com/example/baikiemtragki/ProductServlet.java
@@ -0,0 +1,47 @@
+package com.example.baikiemtragki;
+
+public class ProductServlet {
+    private String name;
+    private  double price;
+    private String description;
+    private  String urlImage;
+
+    public ProductServlet(String name, double price, String description, String urlImage) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.urlImage = urlImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
